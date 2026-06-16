d2f1d1343561b9e5e6cce697ca098f79b480479b
Jean Jacques <jeanjacques7.jj@gmail.com>
2026-06-16 16:02:05 -0300

    BL-01 - Updating bat file to run migrations

--- start-db.bat
+++ start-db.bat
@@ -5,3 +5,15 @@ docker run --name orders-postgres ^
   -e POSTGRES_DB=ordersdb ^
   -p 5432:5432 ^
   -d postgres:16
+
+echo Waiting for PostgreSQL to be ready...
+:wait
+docker exec orders-postgres pg_isready -U postgres >nul 2>&1
+if errorlevel 1 (
+  timeout /t 1 /nobreak >nul
+  goto wait
+)
+
+echo Running migrations...
+docker exec -i orders-postgres psql -U postgres -d ordersdb < src\OrdersApi\Data\Migrations\V1__InitialCreate.sql
+echo Done.
